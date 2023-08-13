ca176a1590384c43a6a7976736020d256fdf1ed5
jod431047 <jod431047@gmail.com>
2023-08-14 00:16:17 +0200

    add product video & working on home

--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from products.models import Product ,Brand , Review
 
-# Create your views here.
+
+
+
+def home(request):
+    brands =Brand.objects.all()
+    sale_products = Product.objects.filter(flag='Sale')[:10]
+    feature_products = Product.objects.filter(flag='Feature')[:6]
+    new_products = Product.objects.filter(flag='New')[:6]
+    reviews = Review.objects.all() 
+    
+    
+    return render(request,'settings/home.html',{
+        'brands':brands ,
+        'sale_products':sale_products,
+        'feature_products':feature_products,
+        'new_products':new_products,
+        'reviews':reviews,
+        'brands_sliced' : brands[:6],
+    })
---
+++ todo.md
@@ -0,0 +1,20 @@
+- sql
+- add review
+- company model
+- show company data
+- setup navbar , footer
+- add orders
+- add cart
+- add users
+- add api
+- queryset api
+- caching
+- celery
+- redis
+- docker
+- coupons
+- payments
+- charts
+- deploy
+- ajax
+- translation
