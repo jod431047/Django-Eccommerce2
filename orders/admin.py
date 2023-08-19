2b4ff0c8d931a5ecc273b34a2cc2a292c9c9cf35
jod431047 <jod431047@gmail.com>
2023-08-19 21:59:02 +0200

    add cart_context_processor v1 , add orders listview

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Order , OrderDetail , Cart , CartDetail , Coupon
-
-
+# Register your models here.
 admin.site.register(Order)
 admin.site.register(OrderDetail)
 admin.site.register(Cart)
