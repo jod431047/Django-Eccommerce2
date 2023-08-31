9f353d818b0e2787fff0593eb353c44ca968a0c0
jod431047 <jod431047@gmail.com>
2023-08-31 18:36:05 +0200

    show all related inn serializer

--- products/serializers.py
+++ products/serializers.py
@@ -3,11 +3,7 @@
 from rest_framework import serializers
 from .models import Product , Brand
 
-class ProductSerializer(serializers.ModelSerializer):
-    brand = serializers.StringRelatedField()
-    class Meta:
-        model = Product
-        fields = '__all__'
+
         
         
         
@@ -17,4 +13,12 @@ class ProductSerializer(serializers.ModelSerializer):
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
+        fields = '__all__'        
+        
+        
+        
+class ProductSerializer(serializers.ModelSerializer):
+    brand = BrandSerializer()
+    class Meta:
+        model = Product
         fields = '__all__'        
