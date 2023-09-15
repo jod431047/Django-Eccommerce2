6014f97a2d5c824c7727bb88529db5c571ee640d
jod431047 <jod431047@gmail.com>
2023-09-15 22:05:04 +0200

    add cart add or update

--- orders/api.py
+++ orders/api.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Cart , Order , OrderDetail , CartDetail
 from . serializers import CartDetailSerializer ,CartSerializer
 from django.contrib.auth.models import User
+from products.models import Product
 
 
 
@@ -14,8 +15,17 @@ class CartDetailCreateDeleteAPI(generics.GenericAPIView):
         return Response({'Cart':data ,'Status':200} )
     
     def post(self,request,*args,**kwargs):
-        pass
-    
+        user = User.objects.get(username=self.kwargs['username'])
+        product = Product.objects.get(id=request.data['product_id'])
+        quantity = int(request.data['quantity'])
+        
+        cart = Cart.objects.get(user=user,completed=False)
+        cart_data , created = CartDetail.objects.get_or_create(cart=cart,product=product)
+        cart_data.price = product.price
+        cart_data.quantity = quantity
+        cart_data.total = round(quantity*product.price,2)
+        cart_data.save()
+        return Response({'status':200 , 'message':'product was added successfully'})
     
     def delete(self,request,*args,**kwargs):
         pass
