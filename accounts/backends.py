78141094977c32998c03191ab21b3f364978fdba
jod431047 <jod431047@gmail.com>
2023-10-15 00:58:15 +0200

    add login with email or username

--- accounts/backends.py
+++ accounts/backends.py
@@ -10,15 +10,18 @@ class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try :
             user = User.objects.get(
-                Q(username__iexact = username)
+                Q(username__iexact = username),
                 Q(email__iexact = username)
                 
             )
         except User.DoesNotExist:
             pass
         
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
         
         
         
         
-      #return super().authenticate(request, username, password, **kwargs)
+    #return super().authenticate(request, username, password, **kwargs)
